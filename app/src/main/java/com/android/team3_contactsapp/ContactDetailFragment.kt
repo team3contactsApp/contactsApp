1ba7a86b99774ddb564169f3fd06d46b51582867
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-01-17 08:25:06 +0900

    추가- name, phoneNumber, memberImg 데이터를 받을 수있게 추가

--- app/src/main/java/com/android/team3_contactsapp/ContactDetailFragment.kt
+++ app/src/main/java/com/android/team3_contactsapp/ContactDetailFragment.kt
@@ -3,12 +3,10 @@ package com.android.team3_contactsapp
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.telecom.Call
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.android.team3_contactsapp.databinding.FragmentContactDetailBinding
 
