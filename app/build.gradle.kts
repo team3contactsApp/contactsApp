9af51adb1ba0f19c955090756eb0d2e163cddd07
MAXIMTHEHARU <150433722+MAXIMTHEHARU@users.noreply.github.com>
2024-01-16 13:14:49 +0900

    Revert "Mypage"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-//    implementation ("com.sothree.slidinguppanel:library:3.4.0")
     implementation("androidx.fragment:fragment-ktx:1.5.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
