30c34d851cf62ca394899321ae6c348eeea01b43
CodingVirus <twominyong@gmail.com>
2024-01-17 17:33:27 +0900

    [추가] - 그룹 모집 페이지 이중 리사이클러뷰 추가

--- app/src/main/java/com/android/team3_contactsapp/group_adapter/GroupRecyclerAdapter2.kt
+++ app/src/main/java/com/android/team3_contactsapp/group_adapter/GroupRecyclerAdapter2.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.android.team3_contactsapp.Data
 import com.android.team3_contactsapp.Group
 import com.android.team3_contactsapp.databinding.RecyclerviewItemGroupBinding
 import com.android.team3_contactsapp.databinding.RecyclerviewItemJoinedgroupBinding
@@ -11,9 +12,9 @@ import com.android.team3_contactsapp.databinding.RecyclerviewItemJoinedgroupBind
 class GroupRecyclerAdapter2(private val dataList: MutableList<String>) : RecyclerView.Adapter<GroupRecyclerAdapter2.Holder>(){
 
     inner class Holder(private val binding: RecyclerviewItemJoinedgroupBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
-            binding.ivJoinedGroupImg
-            binding.tvJoinedGroupName
+        fun bind(str: String) {
+            binding.ivJoinedGroupImg.setImageResource(Data.member[str.toInt()-1].MemberImg)
+            binding.tvJoinedGroupName.setText(Data.member[str.toInt()-1].Name)
         }
     }
 
@@ -25,6 +26,6 @@ class GroupRecyclerAdapter2(private val dataList: MutableList<String>) : Recycle
     override fun getItemCount() = dataList.size
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-
+        dataList[position].run { holder.bind(this) }
     }
 }
