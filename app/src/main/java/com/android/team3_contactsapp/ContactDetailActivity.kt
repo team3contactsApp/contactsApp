2fdde060b286bd71b2d66bd534d033fe5fc7d9c4
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-01-18 21:30:37 +0900

    [수정] ContactDetailActivity 수정

--- app/src/main/java/com/android/team3_contactsapp/ContactDetailActivity.kt
+++ app/src/main/java/com/android/team3_contactsapp/ContactDetailActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -153,13 +154,5 @@ class ContactDetailActivity : AppCompatActivity(), UpdateInfoListener{
     private fun sendCall(phoneNumber: String) {
         val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
         startActivity(intent)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
