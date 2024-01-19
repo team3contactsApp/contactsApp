9b0abb93a04555e753074b1f8ff8a90d2199b034
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-01-19 11:07:14 +0900

    [수정] ContactDetailActivity 수정

--- app/src/main/java/com/android/team3_contactsapp/ContactDetailActivity.kt
+++ app/src/main/java/com/android/team3_contactsapp/ContactDetailActivity.kt
@@ -11,7 +11,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
